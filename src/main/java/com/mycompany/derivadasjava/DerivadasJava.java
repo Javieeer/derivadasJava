905c176e164c9a8b4ae2ca25d17a55691f6ce20b
Javier Zapata <zapatajavier34@gmail.com>
2025-04-30 09:14:41 -0500

    Se trabaja en regla de la potencia

--- src/main/java/com/mycompany/derivadasjava/DerivadasJava.java
+++ src/main/java/com/mycompany/derivadasjava/DerivadasJava.java
@@ -6,14 +6,11 @@ package com.mycompany.derivadasjava;
  * @author javiz
  */
 
-import java.util.*;
-import java.util.regex.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.*;
 import javax.swing.*;
-import net.objecthunter.exp4j.Expression;
-import net.objecthunter.exp4j.ExpressionBuilder;
 
 public class DerivadasJava {
 
@@ -21,7 +18,7 @@ public class DerivadasJava {
         
         // Ventana principal
         JFrame ventanaPrincipal = new JFrame( "Derivadas del mejor Cipas" );
-        ventanaPrincipal.setSize( 800, 600 );
+        ventanaPrincipal.setSize( 800, 300 );
         ventanaPrincipal.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         ventanaPrincipal.setLayout( new BorderLayout( 20, 20 ) );
         ventanaPrincipal.getContentPane().setBackground( new Color( 245, 245, 245 ) ); // Color claro para la ventana
@@ -102,17 +99,19 @@ public class DerivadasJava {
             Expression exp = new ExpressionBuilder( funcion )
                     .variable( "x" )
                     .build();*/
-
-            // Comenzamos a evaluar cada regla
-            String funcionSinConstantes = reglaDeConstante( funcion );
             
-            return funcionSinConstantes;
+            // Comenzamos a evaluar cada regla
+            String funcionConstante = reglaDeConstante( funcion );
+            String funcionPotencias = reglaDePotencia( funcionConstante );
+            
+            return funcionPotencias;
             
         } catch ( Exception e ) {
             return "Error en derivada.";
         }
     }
     
+    // Metodo para encontrar las constantes en una funcion
     public static String encontrarConstantes( String funcion ){
         
         // Creamos la variable para almacenar la constante
@@ -132,23 +131,129 @@ public class DerivadasJava {
                 constante = termino;
             }
         }
-
+        
         return constante;
     }
     
+    // Metodo para encontrar las potencias por ejemplo: 4x^2
+    public static String encontrarPotencias( String funcion ) {
+        
+        String potencia = "";
+
+        // Eliminamos espacios
+        funcion = funcion.replace( " ", "" ); 
+
+        // Detectamos potencias con coeficientes
+        Pattern pattern = Pattern.compile( "[-+]?\\d*\\.?\\d*x\\^\\d+" );
+        Matcher matcher = pattern.matcher( funcion );
+
+        while ( matcher.find() ) {
+            potencia = matcher.group();
+        }
+
+        return potencia;
+    }
+    
+    // Metodo para encontrar potencias elevadas a 1 por ejemplo 5x
+    public static String encontrarPotenciaSinExponente ( String funcion ){
+        
+        String potencia = "";
+
+        // Eliminamos espacios
+        funcion = funcion.replace( " ", "" ); 
+
+        // Detectamos potencias a la 1 con coeficientes
+        Pattern pattern = Pattern.compile( "[-+]?\\d*\\.?\\d*x(?!\\^)" );
+        Matcher matcher = pattern.matcher( funcion );
+
+        while ( matcher.find() ) {
+            potencia = matcher.group();
+        }
+        
+        return potencia;
+    }
+    
+    // Metodo para evaluar regla de la constante
     public static String reglaDeConstante( String funcion ){
         
         // Volvemos a eliminar espacios en caso de tenerlos en la función
-        funcion = funcion.replace(" ", "");
+        funcion = funcion.replace( " ", "" );
         String constante = encontrarConstantes( funcion );
         
         // Limpiamos espacios de la constante en caso de tenerlos
-        constante = constante.replace(" ", "");
-        String funcionSinConstante = funcion.replace(constante, "");
-        System.out.println(funcionSinConstante);
+        constante = constante.replace( " ", "" );
+        String funcionSinConstante = funcion.replace( constante, "" );
         
         return funcionSinConstante;
     }
     
-    
+    // Metodo para evaluar la regla de potencia
+    public static String reglaDePotencia( String funcion ){
+        
+        // Limpiamos la función en caso de que tenga espacios
+        funcion = funcion.replace( " ", "" );
+        String potencia = encontrarPotencias( funcion );
+        String copiaPotencia = potencia;
+        String potenciaSinExpotente = "";
+        String copiaPotenciaSinExpotente = "";
+        System.out.println(!encontrarPotenciaSinExponente( funcion ).isEmpty());
+        if ( !encontrarPotenciaSinExponente( funcion ).isEmpty() ){
+            potenciaSinExpotente = encontrarPotenciaSinExponente( funcion );
+            copiaPotenciaSinExpotente = potenciaSinExpotente;
+        }
+        
+        
+        //Limpiamos la potencia en caso de que tenga espacios
+        potencia = potencia.replace( " ", "" );
+        
+        // Hallamos la potencia y la ponemos de primero reducida 1 unidad
+        int posicionPotencia = funcion.indexOf("^") + 1;
+        String exponente = potencia.substring( posicionPotencia, posicionPotencia + 1 );
+        potencia = exponente + potencia.substring( 0, potencia.length() - 1 );
+        int exponenteNum = Integer.parseInt(exponente) - 1;
+        exponente = String.valueOf(exponenteNum);
+        potencia = potencia + exponente;
+        String potenciaTemp = potencia;
+        
+        // Recorremos la función para detectar números seguidos
+        for ( int i = 0; i < potencia.length() - 1; i++ ) {
+            
+            char actual = potencia.charAt( i );
+            char siguiente = potencia.charAt( i + 1 );
+            
+            // Evaluamos si ambos son números y estan seguidos
+            if ( Character.isDigit( actual ) && Character.isDigit( siguiente ) ){
+                // Creamos variable para el resultado
+                int resultado = 1;
+
+                // Multiplicamos siempre que hallan múmeros seguidos
+                resultado *= Character.getNumericValue( potencia.charAt( i ) );
+                resultado *= Character.getNumericValue( potencia.charAt( i + 1 ) );
+                
+                // Borramos los números de potencia
+                int ubicaciontemp = potenciaTemp.indexOf( potencia );
+                potenciaTemp = potenciaTemp.substring( ubicaciontemp + 2 );
+                String resultadoTemp = String.valueOf( resultado );
+                potencia = resultadoTemp + potenciaTemp;
+                i++;
+            }
+        }
+        
+        // Evaluamos si la potencia es 1 entonces no la nombramos y dejamos la variable sola
+        posicionPotencia = potencia.indexOf( "^" ) + 1;
+        if ( potencia.charAt( posicionPotencia ) == '1' ){
+            potencia = potencia.substring( 0, potencia.length() - 2 );
+        }
+        
+        String funcionTemp = funcion.replace( copiaPotencia, potencia );
+        
+        // Tratamos potencias elexadas a la 1
+        if ( !encontrarPotenciaSinExponente( funcion ).isEmpty() ){
+            potenciaSinExpotente = potenciaSinExpotente.substring( 0, potenciaSinExpotente.length() - 1 );
+            funcionTemp = funcionTemp.replace(copiaPotenciaSinExpotente, potenciaSinExpotente);
+        }
+        
+        
+        return funcionTemp;
+    }
 }
